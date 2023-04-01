c2e48ec01c94814575e7f4baf1e32083c3e0344b
kealfeyne <kealfeyneworkshop@gmail.com>
2023-04-01 19:36:18 +0300

    Add retail map

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import geopandas as gpd
+import folium
 import matplotlib.pyplot as plt
 import requests
 from models.ranking import Ranking
@@ -40,13 +42,47 @@ with analytics_tab:
 
 with ranking_tab:
     ranker = Ranking()
-    product_type = st.text_input("Вид товара", value="9199AB529CF62D4BDB7E8B1D7459001D")
-    ranked_data = ranker.ranking(product_type)
-    if ranked_data is not None:
-        if len(ranked_data) == 0:
-            st.warning('Недостаточно данных')
-        else:
-            st.dataframe(ranked_data)
+    ranker.group_region_retails()
+    # product_type = st.text_input("Вид товара", value="9199AB529CF62D4BDB7E8B1D7459001D")
+    # ranked_data = ranker.ranking(product_type)
+    # if ranked_data is not None:
+    #     if len(ranked_data) == 0:
+    #         st.warning('Недостаточно данных')
+    #     else:
+    #         st.dataframe(ranked_data)
 
 with tab3:
-    st.header("Tab 3 Content")
+    # загружаем данные о границах и именах регионов
+    russia_regions = gpd.read_file('admin_level_4.geojson')
+
+    # добавляем уникальные id'шники регионов
+    russia_regions['REGION_ID'] = np.arange(0, 88)
+
+    # создаём датафрейм со значениями в каждом регионе
+    df = pd.DataFrame()
+    df['REGION_ID'] = np.arange(0, 88)
+    df['value'] = np.linspace(0.0, 1000.0, num=88)
+
+    m = folium.Map(location=[63.391522, 96.328125], zoom_start=3, tiles='cartodb positro')
+    # cartodb positro - весит 27 МБ
+
+    rel_ = folium.Choropleth(
+        geo_data=russia_regions,
+        name='Регионы России',
+        data=df,
+        columns=['REGION_ID', 'value'],
+        key_on='feature.properties.REGION_ID',
+        bins=5,
+        fill_color='BuGn',
+        nan_fill_color='darkblue',
+        nan_fill_opacity=0.5,
+        fill_opacity=0.7,
+        line_opacity=0.2,
+        legend_name='Регионы России',
+        highlight=True,
+        show=False
+    )
+
+    rel_.add_to(m)
+
+    m.save('map.html')
--- models/ranking.py
+++ models/ranking.py
@@ -10,6 +10,7 @@ class Ranking:
         # TODO: вынести в конфиг
         path_to_closings: str = "data/Output_short.parquet"  # "data/Output.parquet"
         path_to_products: str = "data/Products.csv"
+        path_to_retails: str = "data/Places.csv"
 
         self.gtin_column = ["gtin"]
         self.sale_fields = ['Продажа конечному потребителю в точке продаж',
@@ -20,6 +21,7 @@ class Ranking:
 
         self.closings = pd.read_parquet(path_to_closings, engine="fastparquet")
         self.products = pd.read_csv(path_to_products)
+        self.retails = pd.read_csv(path_to_retails)
 
     def extract_prices(self, x):
         # Цены на gtin у каждого производителя
@@ -28,80 +30,94 @@ class Ranking:
 
         return x
 
-    def extract_features(self, product_type: str):
+    def group_region_retails(self, product_gtin: str = "1248F88441BCFC563FB99D77DB0BB80D"):
         # gtin товаров определенной категории
-        product_ids = self.products[self.products['product_short_name'] == product_type]['gtin']
+        # product_ids = self.products[self.products['product_short_name'] == product_type]['gtin']
 
-        # Проданные товары определенной категории
-        sales = self.closings[
-            self.closings['gtin'].isin(product_ids) & self.closings['type_operation'].isin(self.sale_fields)]
+        # Проданные товары определенного gtin
+        sales = self.closings[(self.closings['gtin'] == product_gtin) &
+                              self.closings['type_operation'].isin(self.sale_fields)]
 
-        # Поставщики
-        prids = list(sales.groupby("prid").groups.keys())
+        # Продажи с городами точек продаж
+        sales = sales.merge(self.retails.drop(["inn"], axis=1), left_on='id_sp_', right_on='id_sp_')  # ['id_sp', 'inn', 'region_code']
 
-        # Разнообразие gtin у каждого производителя
-        variety = sales.groupby("prid", group_keys=False).apply(lambda x: x["gtin"].unique().shape[0])
+        # Выделение фич
+        grouped = sales.groupby("region_code", group_keys=True)
+        # regions = list(groups.keys())
 
-        # Объемы продаж в единицах товаров
-        counts = sales.groupby("prid", group_keys=False)["cnt"].sum()
+        min_places = []
+        max_places = []
+        for name, group in grouped:
+            min_places.append([sales.iloc[group["price"].idxmin()]["inn"]])
+            max_places.append([sales.iloc[group["price"].idxmax()]["inn"]])
 
-        # Цены проданных товаров
-        prices = sales.groupby("prid", group_keys=False).apply(lambda x: self.extract_prices(x))
+        result = {"regions": list(grouped.groups.keys()),
+                  "mean_prices": grouped["price"].mean(),
+                  "min_prices": grouped["price"].min(),
+                  "min_places": min_places,
+                  "max_prices": grouped["price"].max(),
+                  "max_places": max_places}
 
-        return list(zip(prids, variety, counts, prices))
+        print(pd.DataFrame(result))
+
+        return result
 
     def ranking(self, product_type: str = "9199AB529CF62D4BDB7E8B1D7459001D"):
         ranked_list = self.extract_features(product_type)
-        for i in tqdm(range(0, len(ranked_list) - 1)):
-            for index in range(len(ranked_list) - i - 1):
-                # Разнообразие больше следующего
-                variety_larger_score = int(
-                    ranked_list[index][self.fields_mapping["variety"]] > ranked_list[index + 1][
-                        self.fields_mapping["variety"]])
 
-                # Объем продаж больше следующего
-                counts_larger_score = int(
-                    ranked_list[index][self.fields_mapping["counts"]] > ranked_list[index + 1][
-                        self.fields_mapping["counts"]])
+    # def extract_features(self, product_gtin: str):
+    # # gtin товаров определенной категории
+    # # product_ids = self.products[self.products['product_short_name'] == product_type]['gtin']
+    #
+    # # Проданные товары определенного gtin
+    # sales = self.closings[
+    #     (self.closings['gtin'] == product_gtin) & self.closings['type_operation'].isin(self.sale_fields)]
+    #
+    # # Производители
+    # prids = list(sales.groupby("prid").groups.keys())
+    #
+    # # Разнообразие gtin у каждого производителя
+    # variety = sales.groupby("prid", group_keys=False).apply(lambda x: x["gtin"].unique().shape[0])
+    #
+    # # Объемы продаж в единицах товаров
+    # counts = sales.groupby("prid", group_keys=False)["cnt"].sum()
+    #
+    # # Цены проданных товаров
+    # prices = sales.groupby("prid", group_keys=False).apply(lambda x: self.extract_prices(x))
+    #
+    # return list(zip(prids, variety, counts, prices))
 
-                # Сравнение цен на одинаковые товары
-                same_products = ranked_list[index][self.fields_mapping["prices"]].keys() & ranked_list[index + 1][
-                    self.fields_mapping["prices"]].keys()
-
-                counter = 0
-                for product in same_products:
-                    # Если цена выше -> поставщик лучше
-                    if ranked_list[index][self.fields_mapping["prices"]][product] > \
-                            ranked_list[index + 1][self.fields_mapping["prices"]][product]:
-                        counter += 1
-                # Часть лучших продуктов среди общих
-                prices_benefits_score = counter / max(1, len(same_products))
-
-                # Общий скор
-                score = variety_larger_score + counts_larger_score + prices_benefits_score
-
-                if score > 1:
-                    ranked_list[index], ranked_list[index + 1] = ranked_list[index + 1], ranked_list[index]
-
-        df = pd.DataFrame(reversed(ranked_list), columns=['Prid', 'Variety', 'Volume', 'Prices']) # .astype({'name1': int})
-        # # Временно
-        # df = pd.DataFrame()
-        #
-        # a1 = []
-        # a2 = []
-        # a3 = []
-        # a4 = []
-        #
-        # for i in reversed(ranked_list):
-        #     a1.append(i[0])
-        #     a2.append(i[1])
-        #     a3.append(i[2])
-        #     a4.append(i[3])
-        #
-        # df['Prid'] = a1
-        # df['Variety'] = a2
-        # df['Volume'] = a3
-        # df['Prices'] = a4
-
-        return df
-        # return json.dumps([{"prid": item[0], "variety": item[1], "volume": item[2]} for item in ranked_list])
+    # def ranking(self, product_type: str = "9199AB529CF62D4BDB7E8B1D7459001D"):
+    #     ranked_list = self.extract_features(product_type)
+    #     for i in tqdm(range(0, len(ranked_list) - 1)):
+    #         for index in range(len(ranked_list) - i - 1):
+    #             # Разнообразие больше следующего
+    #             variety_larger_score = int(
+    #                 ranked_list[index][self.fields_mapping["variety"]] > ranked_list[index + 1][
+    #                     self.fields_mapping["variety"]])
+    #
+    #             # Объем продаж больше следующего
+    #             counts_larger_score = int(
+    #                 ranked_list[index][self.fields_mapping["counts"]] > ranked_list[index + 1][
+    #                     self.fields_mapping["counts"]])
+    #
+    #             # Сравнение цен на одинаковые товары
+    #             same_products = ranked_list[index][self.fields_mapping["prices"]].keys() & ranked_list[index + 1][
+    #                 self.fields_mapping["prices"]].keys()
+    #
+    #             counter = 0
+    #             for product in same_products:
+    #                 # Если цена выше -> поставщик лучше
+    #                 if ranked_list[index][self.fields_mapping["prices"]][product] > \
+    #                         ranked_list[index + 1][self.fields_mapping["prices"]][product]:
+    #                     counter += 1
+    #             # Часть лучших продуктов среди общих
+    #             prices_benefits_score = counter / max(1, len(same_products))
+    #
+    #             # Общий скор
+    #             score = variety_larger_score + counts_larger_score + prices_benefits_score
+    #
+    #             if score > 1:
+    #                 ranked_list[index], ranked_list[index + 1] = ranked_list[index + 1], ranked_list[index]
+    #
+    #     return pd.DataFrame(reversed(ranked_list), columns=['Prid', 'Variety', 'Volume', 'Prices'])
